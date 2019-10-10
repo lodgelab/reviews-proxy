ec4896a3bd54f05e3cafb58cef7507a3697aedd2
alvinlo92 <alvinlo92@yahoo.com>
2019-10-09 17:07:09 -0700

    Add compression for express server
    Update links to new AWS EC2 instance

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 require('newrelic');
+const proxy = require('express-http-proxy');
+const compression = require('compression');
 const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
 
+app.use(compression());
 app.use(morgan('tiny'));
 app.use('/listings/:property', express.static('public'));
 app.use(express.static('public'));
+
+app.get('/api/listings/:property/reviews', proxy('http://52.53.213.247'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
